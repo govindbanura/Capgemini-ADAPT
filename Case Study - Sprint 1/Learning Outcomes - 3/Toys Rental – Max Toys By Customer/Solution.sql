dba45b8d69d536f3263597a4d863a55c61e3519a
Govind Banura <govindbanura2310@gmail.com>
2021-01-22 18:49:25 +0530

    Update Solution.sql

--- Case Study - Sprint 1/Learning Outcomes - 3/Toys Rental – Max Toys By Customer/Solution.sql
+++ Case Study - Sprint 1/Learning Outcomes - 3/Toys Rental – Max Toys By Customer/Solution.sql
@@ -1,7 +1,4 @@
 alter session set current_schema = mt_toy;
 SET heading OFF
 
-SELECT DISTINCT CUSTOMER_ID FROM TOY_RENTAL
-JOIN TOY
-ON TOY_RENTAL.TOY_ID = TOY.TOY_ID
-WHERE CUSTOMER_ID = 1010;
+select id CUSTOMER_ID from (select c.Customer_Id id, sum(t.Quantity) q from Customer c, Toy_Rental tr, Toy t where c.Customer_Id = tr.Customer_ID AND tr.Toy_ID = t.Toy_Id group by c.Customer_Id order by q desc) where rownum = 1;
