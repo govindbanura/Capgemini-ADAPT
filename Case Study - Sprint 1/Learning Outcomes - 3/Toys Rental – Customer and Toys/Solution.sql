09f9b081e7ad5284c940589df4941a6ec1c9f146
Govind Banura <govindbanura2310@gmail.com>
2021-01-22 18:42:15 +0530

    Update Solution.sql

--- Case Study - Sprint 1/Learning Outcomes - 3/Toys Rental – Customer and Toys/Solution.sql
+++ Case Study - Sprint 1/Learning Outcomes - 3/Toys Rental – Customer and Toys/Solution.sql
@@ -1,6 +1,6 @@
 alter session set current_schema = mt_toy;
 SET heading OFF
 
-SELECT CUSTOMER_NAME, CITY, RENTAL_ID, RENTAL_START_DATE, RENTAL_END_DATE
+SELECT CUSTOMER_NAME, CITY, RENTAL_ID, to_char(to_date(R.Rental_Start_Date,'DD-MM-YY'), 'DD-MM-YY') RENTAL_START_DATE,  to_char(to_date(R.Rental_End_Date,'DD/MM/YYYY'), 'DD-MM-YY') RENTAL_END_DATE 
 FROM CUSTOMER C, TOY_RENTAL R
-WHERE C.CUSTOMER_ID = R.CUSTOMER_ID(+);
+WHERE C.CUSTOMER_ID = R.CUSTOMER_ID(+) order by C.Customer_ID ASC;
